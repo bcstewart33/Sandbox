7f6b64583e9dd4592e7610698944ba4f890d3dfb
Bryan Stewart <bryan.stewart@10xgenomics.com>
2017-11-17 09:01:03 -0800

    Added Closing

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -10,11 +10,19 @@ public class HelloWorld {
 
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
+
         Salutation ("by bryan.stewart@10xgenomics.com");
+
+        Closing ("10X Genomics Inc");
     }
 
     public static void Salutation (String str) {
 
         System.out.println("Salutation: " + str);
+    }
+
+    public static void Closing (String str) {
+
+        System.out.println("Closing: " + str);
     }
 }
